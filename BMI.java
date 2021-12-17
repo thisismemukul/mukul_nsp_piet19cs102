f307bc39befaa211e28b64c6b9f87ef9026a60ce
Mukul Saini <thisismemukul@gmail.com>
2021-12-17 19:05:17 +0530

    first commit

---
+++ BMI.java
@@ -0,0 +1,45 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+/*<applet code="BMI.class" width="900" height="900"></applet>*/
+public class BMI extends Applet implements ActionListener{
+	TextField t1 = new TextField(10);
+	TextField t2 = new TextField(10);
+	Label l1 = new Label("Please enter your weight in kg: ");
+	Label l2 = new Label("Please enter your height in cm: ");
+	Button b1 =new Button("Calculate my BMI");
+	Label l3 = new Label("");
+
+	public void init(){
+		add(l1);
+		add(t1);
+		add(l2);
+		add(t2);
+		add(b1);
+		add(l3);
+		b1.addActionListener(this);
+	}
+	public void actionPerformed(ActionEvent  ae){
+		if(ae.getSource()==b1){
+			float n1 = Float.parseFloat(t1.getText());
+			float n2 = Float.parseFloat(t2.getText());
+			 l3.setLocation(300,300);
+			l3.setSize(300,300);
+			 l3.setFont(new Font("TimesRoman", Font.BOLD, 25));
+		float bmi = (100*100*n1)/(n2*n2);
+		if(bmi < 18.5) {
+			 l3.setForeground(Color.blue);
+				l3.setText("Your BMI is: "+bmi+" You are underweight");
+			}else if (bmi < 25) {
+			 l3.setForeground(Color.green);
+				l3.setText("Your BMI is: "+bmi+" You are normal");
+			}else if (bmi < 30) {
+			 l3.setForeground(Color.yellow);
+				l3.setText("Your BMI is: "+bmi+" You are overweight");
+			}else {
+			 l3.setForeground(Color.red);
+				l3.setText("Your BMI is: "+bmi+" You are obese");
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Java NSP Project
+Que:- Design system for Body Mass Calculation 
+
+created by [Mukul Saini](https://github.com/thisismemukul).
+
+- [Installation Guide](#installation-guide) - How to get started with this project on your system.
+
+# <a name='installation-guide'>Installation Guide</a>
+
+This project requires the following tools:
+
+- [Java or JDK](https://www.oracle.com/java/technologies/downloads/#java8-windows) - Java 8 or JDK 1.8.
+
+To get started, install Java or JDK on your local computer if you don't have them already.
+
+## Getting Started
+
+**Step 1. Clone the code into a fresh folder**
+
+```
+$ git clone https://github.com/thisismemukul/mukul_nsp_piet19cs102.git
+$ cd mukul_nsp_piet19cs102
+```
+
+**Step 2. Run file using following commands.**
+
+Use `CMD` or `Terminal`, to run the following commands.
+
+```
+$ javac BMI.java
+```
+
+```
+$ appletviewer BMI.java
+```
